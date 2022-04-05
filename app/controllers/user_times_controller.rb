d9957e3038de8575e776476bad73c9fe1b004f69
Montensia Banks <montensia@gmail.com>
2022-04-04 21:27:25 -0500

    Added update method to controller

--- app/controllers/user_times_controller.rb
+++ app/controllers/user_times_controller.rb
@@ -21,8 +21,21 @@ class UserTimesController < ApplicationController
         else 
             render json: {err_message: "A user is not logged in."}
         end
-     
-  
+    end 
+
+    def update
+   
+        user = User.find_by({id: params[:user_id]})
+        if user
+            user_time_date = user.user_times.find_by({date_of_times: params[:date_of_times]})
+            date = "#{params[:date_of_times]}"
+            clock_in = "#{params[:clock_in]} #{date}" #constructing dateTime
+            clock_out = "#{params[:clock_out]} #{date}" #constructing dateTime
+            user_time_date.update(clock_in: clock_in, clock_out: clock_out)
+            render json: user, include: :user_times
+        else 
+            render json: {err_message: "A user is not logged in."}
+        end
     end
- 
+  
 end
