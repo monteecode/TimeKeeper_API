33c76ef6c92425dcd19b13ad5b0fd62affda2e51
Montensia Banks <montensia@gmail.com>
2022-04-05 19:59:51 -0500

    Fixed CORS error

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,14 @@ Bundler.require(*Rails.groups)
 
 module TimekeeperApi
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+          origins '*'
+          resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
